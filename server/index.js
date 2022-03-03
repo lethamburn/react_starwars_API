a446d5e05b41fece209877ffa97e3c8a5b3505ac
Antonio Rosales <75122333+lethamburn@users.noreply.github.com>
2022-03-03 17:41:02 +0100

    REFACTOR FONT

--- server/index.js
+++ server/index.js
@@ -34,6 +34,12 @@ app.use(
 
 app.all("/", (req, res) => {
   res.send(`
+  <head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Star Wars - Node Server</title>
+</head>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Chakra+Petch:wght@400;500;700&display=swap" rel="stylesheet">
