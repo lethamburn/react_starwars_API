95fb31c741c4b3017ce15a9bef49fbadcc8f3076
Antonio Rosales <75122333+lethamburn@users.noreply.github.com>
2022-03-17 09:55:30 +0100

    FIX Static Props

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 
-const Home = ({characters, movies, planets}) => {
+const Home = ({characters}) => {
   //console.log("CHARACTERS->", characters.data.characters);
   //console.log("MOVIES->", movies.data.movies);
   //console.log("PLANETS->", planets.data.planets);
@@ -22,16 +22,16 @@ export async function getStaticProps() {
 
   const resCharacters = await fetch('https://starwars-server.vercel.app/characters')
   const characters = await resCharacters.json();
-  const resMovies = await fetch("https://starwars-server.vercel.app/movies");
+ /*  const resMovies = await fetch("https://starwars-server.vercel.app/movies");
   const movies = await resMovies.json();
   const resPlanets = await fetch("https://starwars-server.vercel.app/planets");
-  const planets = await resPlanets.json();
+  const planets = await resPlanets.json(); */
 
   return {
     props: {
       characters,
-      movies,
-      planets
+    /*   movies,
+      planets */
     },
   }
 }
