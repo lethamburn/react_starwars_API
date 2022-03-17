7a654ddda80ac5796d2ab525bca59b97980bee4d
Antonio Rosales <75122333+lethamburn@users.noreply.github.com>
2022-03-17 09:44:18 +0100

    FEAT static props and main styles

---
+++ client/pages/_app.js
@@ -0,0 +1,86 @@
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+@font-face {
+      font-family: "Anakin";
+      src: url("./fonts/anakin.ttf");
+    };
+@font-face {
+      font-family: "Star Wars";
+      src: url("./fonts/starwars.ttf");
+    };
+@font-face {
+      font-family: "Episode";
+      src: url("./fonts/episode.ttf");
+    };
+@font-face {
+      font-family: "Aurek";
+      src: url("./fonts/aurek.ttf");
+    };
+
+html{
+  margin:0 auto;
+  padding: 0;
+  scroll-behavior: smooth;
+}
+
+body{
+  background-image:url("./images/background.jpg");
+  background-color:#19181d;
+  min-height:100vh;
+  padding: 1rem;
+  margin-top:0;
+  font-family: "Anakin";
+  color: #e6e8e9;
+}
+
+h1{
+  font-family: "Star Wars";
+  color: #fc9d1b;
+}
+;
+h2{
+  color: #f4500a;
+}
+h3{
+  font-family: "Episode"
+};
+h4{
+  font-family: "Aurek";
+  color: #fff;
+  text-shadow:
+      0 0 7px #538397,
+      0 0 10px #538397,
+      0 0 21px #538397,
+      0 0 42px #538397,
+      0 0 82px #538397,
+      0 0 92px #538397,
+      0 0 102px #538397,
+      0 0 151px #538397;
+  transition: 1s;
+  &:hover{
+    font-family: "Anakin";  
+    cursor: crosshair;
+  }
+}
+`;
+
+const theme = {
+  colors: {
+    primary: "#fafafa",
+  },
+};
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default MyApp;
---
+++ client/pages/index.js
@@ -0,0 +1,37 @@
+import Head from "next/head";
+
+const Home = ({characters, movies, planets}) => {
+  //console.log("CHARACTERS->", characters.data.characters);
+  //console.log("MOVIES->", movies.data.movies);
+  //console.log("PLANETS->", planets.data.planets);
+  return (
+    <div>
+      <div className="main"></div>
+      <Head>
+        <title>Star Wars</title>
+        <link rel="icon" href="./images/icon.png" />
+      </Head>
+      <h1>Star Wars</h1>
+    </div>
+  );
+};
+
+export default Home;
+
+export async function getStaticProps() {
+
+  const resCharacters = await fetch('https://starwars-server.vercel.app/characters')
+  const characters = await resCharacters.json();
+  const resMovies = await fetch("https://starwars-server.vercel.app/movies");
+  const movies = await resMovies.json();
+  const resPlanets = await fetch("https://starwars-server.vercel.app/planets");
+  const planets = await resPlanets.json();
+
+  return {
+    props: {
+      characters,
+      movies,
+      planets
+    },
+  }
+}
