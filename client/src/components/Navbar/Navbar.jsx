0db337e02b4ba19da6721e0454bdcf5ee4bf4ad3
Antonio Rosales <75122333+lethamburn@users.noreply.github.com>
2022-04-26 17:49:43 +0200

    FEAT NAVBAR

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -8,16 +8,36 @@ const Navbar = () => {
       <nav>
         <ul className="item-list">
           <li>
-            <Link to="/">Home</Link>
+          <Link to="/">
+              <div className="link">
+                <img src="./icons/Star_Wars_Falcon.svg" alt="" />
+                <p>Home</p>
+              </div>
+            </Link>
           </li>
           <li>
-            <Link to="/">Movies</Link>
+          <Link to="/">
+              <div className="link">
+                <p>Movies</p>
+                <img src="./icons/Star_Wars_R2D2.svg" alt="" />
+              </div>
+            </Link>
           </li>
           <li>
-            <Link to="/">Characters</Link>
+          <Link to="/">
+              <div className="link">
+                <img src="./icons/Star_Wars_Storm-Trooper.svg" alt="" />
+                <p>Characters</p>
+              </div>
+            </Link>
           </li>
           <li>
-            <Link to="/">Planets</Link>
+            <Link to="/">
+              <div className="link">
+                <p>Planets</p>
+                <img src="./icons/Star_Wars_Death Star.svg" alt="" />
+              </div>
+            </Link>
           </li>
         </ul>
       </nav>
