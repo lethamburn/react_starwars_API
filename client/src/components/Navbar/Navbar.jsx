9c6619f5e301924495e32326494d2018f62dd3a2
Antonio Rosales <75122333+lethamburn@users.noreply.github.com>
2022-04-29 13:02:30 +0200

    REFACTOR navbar styles

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -4,29 +4,29 @@ import "./Navbar.scss";
 const Navbar = () => {
   return (
       <nav className="navbar">
-        <Link to="/" className="logo">LOGO</Link>
+        <Link to="/" className="logo"><img src="./icons/deathstar.png" alt="Death Star Icon" /></Link>
         <input type="checkbox" id="toggler" />
         <label htmlFor="toggler"><i className="ri-menu-line"></i></label>
     <div className="menu">
       <ul className="list">
         <li>
           <Link to="/characters">
-            Characters
+          <img src="./icons/vader.png" alt="Darth Vader icon" />Characters
           </Link>
         </li>
         <li>
           <Link to="/movies">
-            Movies
+          <img src="./icons/r2d2.png" alt="R2D2 icon" />Movies
           </Link>
         </li>
         <li>
           <Link to="/planets">
-            Planets
+          <img src="./icons/falcon.png" alt="Millenium Falcon icon" />Planets
           </Link>
         </li>
         <li>
           <Link to="/about">
-            About
+          <img src="./icons/c3po.png" alt="C3PO icon" />About
           </Link>
         </li>
       </ul>
