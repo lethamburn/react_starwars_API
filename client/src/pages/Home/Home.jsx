af24e41aa44ce46b2e7620e0241153e97ea39311
Antonio Rosales <75122333+lethamburn@users.noreply.github.com>
2022-04-29 15:49:21 +0200

    FEAT Home

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -7,6 +7,19 @@ const Home = () => {
         <video src="./images/video.mp4" autoPlay loop muted></video>
         <h1 className="title">STAR WARS</h1>
       </section>
+      <div className="hero-container">
+        <section className="hero">
+          <p>
+            <strong>Hello there!</strong>
+            <br /> This web application is the front end of the server
+            previously created in <strong> Node/Express</strong> with which to
+            consult information about the movies, characters and planets of the 
+            <strong> Star Wars</strong> saga. <br />
+            Feel free to search and share suggestions with me to improve the
+            information currently in the database.
+          </p>
+        </section>
+      </div>
     </div>
   );
 };
