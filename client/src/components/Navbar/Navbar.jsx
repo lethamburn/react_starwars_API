686381eae146d39bf177865d6337ecb0d6d1e76c
Antonio Rosales <75122333+lethamburn@users.noreply.github.com>
2022-04-29 12:17:56 +0200

    FEAT title and navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,47 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { slide as Menu } from "react-burger-menu";
 import "./Navbar.scss";
 const Navbar = () => {
   return (
-    <Menu>
-      <nav>
-        <ul className="item-list">
-          <li>
-          <Link to="/">
-              <div className="link">
-                <img src="./icons/Star_Wars_Falcon.svg" alt="" />
-                <p>Home</p>
-              </div>
-            </Link>
-          </li>
-          <li>
-          <Link to="/">
-              <div className="link">
-                <p>Movies</p>
-                <img src="./icons/Star_Wars_R2D2.svg" alt="" />
-              </div>
-            </Link>
-          </li>
-          <li>
-          <Link to="/">
-              <div className="link">
-                <img src="./icons/Star_Wars_Storm-Trooper.svg" alt="" />
-                <p>Characters</p>
-              </div>
-            </Link>
-          </li>
-          <li>
-            <Link to="/">
-              <div className="link">
-                <p>Planets</p>
-                <img src="./icons/Star_Wars_Death Star.svg" alt="" />
-              </div>
-            </Link>
-          </li>
-        </ul>
+      <nav className="navbar">
+        <Link to="/" className="logo">LOGO</Link>
+        <input type="checkbox" id="toggler" />
+        <label htmlFor="toggler"><i className="ri-menu-line"></i></label>
+    <div className="menu">
+      <ul className="list">
+        <li>
+          <Link to="/characters">
+            Characters
+          </Link>
+        </li>
+        <li>
+          <Link to="/movies">
+            Movies
+          </Link>
+        </li>
+        <li>
+          <Link to="/planets">
+            Planets
+          </Link>
+        </li>
+        <li>
+          <Link to="/about">
+            About
+          </Link>
+        </li>
+      </ul>
+    </div>
       </nav>
-    </Menu>
+
   );
 };
 
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-
+import React from "react";
+import "./Home.scss";
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <section className="showcase">
+        <video src="./images/video.mp4" autoPlay loop muted></video>
+        <h1 className="title">STAR WARS</h1>
+      </section>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
