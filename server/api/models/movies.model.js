85284ecff4436b131be7fdfb1679eb982cd511da
Antonio Rosales <75122333+lethamburn@users.noreply.github.com>
2022-05-06 11:37:07 +0200

    FEAT movie numbers

--- server/api/models/movies.model.js
+++ server/api/models/movies.model.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const MoviesSchema = new Schema(
   {
     id: { type: Number, required: true },
+    number: {type: String, required: false},
     name: { type: String, required: true },
     year: { type: Number, required: true },
     poster: { type: String, required: true },
